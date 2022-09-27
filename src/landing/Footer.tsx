5fa59fa8ff75faa1f17fa41303025ed7979dedb1
suncom-devin-boyd <devin.boyd@dms.fl.gov>
2022-09-27 14:21:23 -0400

    finished

--- src/landing/Footer.tsx
+++ src/landing/Footer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import {
-  Facebook,
-  Instagram,
-  Twitter,
-  Linkedin,
-  Phone,
+  // Facebook,
+  // Instagram,
+  // Twitter,
+  // Linkedin,
+  // Phone,
   Inbox,
 } from "react-bootstrap-icons";
 
@@ -18,71 +18,28 @@ const Footer = () => {
             <h6>Quick links</h6>
             <ul className="list-unstyled">
               <li>
-                <a href="#0">Home</a>
+                <a href="#0">Top of the Page</a>
               </li>
               <li>
-                <a href="#0">Order Now</a>
+                <a href="https://www.amazon.com/Inhale-Respiratory-Trainer-Breathing-Exercise/dp/B091L441KW/ref=sr_1_5?crid=36QXB5CVFJQ2L&keywords=inhale+breather&qid=1646154061&sprefix=inhale+breather%2Caps%2C87&sr=8-5">
+                  Order Now
+                </a>
               </li>
               <li>
-                <a href="#0">Support</a>
+                <a href="mailto:admin@inhalebreathers.com">Contact Us</a>
               </li>
             </ul>
           </Col>
+          <Col xs={12} sm={6} lg={3}></Col>
+          <Col xs={12} sm={6} lg={3}></Col>
           <Col xs={12} sm={6} lg={3}>
-            <h6>Information</h6>
-            <ul className="list-unstyled">
-              <li>
-                <a href="#0">About us</a>
-              </li>
-              <li>
-                <a href="#0">Contact</a>
-              </li>
-              <li>
-                <a href="#0">Partnership</a>
-              </li>
-            </ul>
-          </Col>
-          <Col xs={12} sm={6} lg={3}>
-            <h6>Follow us</h6>
-            <ul className="list-unstyled">
-              <li>
-                <a href="#0">
-                  <Facebook /> Facebook
-                </a>
-              </li>
-              <li>
-                <a href="#0">
-                  <Instagram /> Instagram
-                </a>
-              </li>
-              <li>
-                <a href="#0">
-                  <Twitter /> Twitter
-                </a>
-              </li>
-              <li>
-                <a href="#0">
-                  <Linkedin /> LinkedIn
-                </a>
-              </li>
-            </ul>
-          </Col>
-          <Col xs={12} sm={6} lg={3}>
-            <h6>Mailing Address</h6>
             <address>
               <strong>Inhale</strong>
-              <br />
-              PO Box 2651
-              <br />
-              Bonita Springs FL 34133
-              <br />
-              <Phone />
-              <span className="visually-hidden">Telephone:</span>{" "}
-              <a href="tel:+12127363100">(305) 823-3100</a>
-              <br />
               <Inbox />
               <span className="visually-hidden">Mail:</span>{" "}
-              <a href="mailto:yttsllc@gmail.com">admin@inhalebreathers.com</a>
+              <a href="mailto:admin@inhalebreathers.com">
+                admin@inhalebreathers.com
+              </a>
             </address>
           </Col>
         </Row>
@@ -91,12 +48,6 @@ const Footer = () => {
             <ul className="list-inline">
               <li className="list-inline-item">2022 Inhale&#8482;</li>
               <li className="list-inline-item">All rights reserved.</li>
-              <li className="list-inline-item">
-                <a href="#0" data-bs-toggle="modal" data-bs-target="#modal">
-                  Terms of use and privacy policy
-                </a>
-                .
-              </li>
             </ul>
           </Col>
         </Row>
